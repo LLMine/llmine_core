b20e520ec615bc04d85c89020b3192c89dccfa00
Arpan Mukherjee <arpan@vulmiqi.com>
2023-09-12 23:18:44 +0530

    fix for superuser auto creation in docker compose

--- docker-entrypoint-celery.sh
+++ docker-entrypoint-celery.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --no-input
+python manage.py createsuperuser --no-input --email admin@example.com
 celery -A llmine_core worker -l info --concurrency=2
